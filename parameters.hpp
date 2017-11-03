918bcf055a898e3989b9d3e135edd987b4e44bf1
Jonas Greitemann <j.greitemann@lmu.de>
2017-11-03 13:39:36 +0100

    Use nu-SVC by default instead of C-SVC

--- parameters.hpp
+++ parameters.hpp
@@ -12,19 +12,16 @@ namespace svm {
         class basic_parameters {
         public:
             basic_parameters (struct svm_parameter const& p) : params(p) {
-                if (p.svm_type != C_SVC) {
-                    std::invalid_argument("SVMs other than C_SVC are not "
+                if (p.svm_type != NU_SVC) {
+                    std::invalid_argument("SVMs other than nu-SVC are not "
                                           "currently supported");
                 }
             };
-            basic_parameters (double C = 1.) {
-                params.svm_type = C_SVC;
+            basic_parameters (double nu = 0.1) {
+                params.svm_type = NU_SVC;
                 params.cache_size = 100;
                 params.eps = 1e-3;
-                params.C = C;
-                params.nr_weight = 0;
-                params.weight_label = NULL;
-                params.weight = NULL;
+                params.nu = 0.1;
                 params.shrinking = 1;
                 params.probability = 0;
             }
