d6d00d3cbfa6b31a9d21591ca5b52939e9a7631a
Jonas Greitemann <j.greitemann@lmu.de>
2018-02-06 13:59:09 +0100

    Silence a warning message in libsvm referring to a non-existing flag

--- libsvm/svm.cpp
+++ libsvm/svm.cpp
@@ -475,9 +475,6 @@ void Solver::reconstruct_gradient()
 		if(is_free(j))
 			nr_free++;
 
-	if(2*nr_free < active_size)
-		info("\nWARNING: using -h 0 may be faster\n");
-
 	if (nr_free*l > 2*active_size*(l-active_size))
 	{
 		for(i=active_size;i<l;i++)
