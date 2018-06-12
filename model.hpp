97e126173227f28e9a6af8ad794cd848b1b924bb
Jonas Greitemann <j.greitemann@lmu.de>
2018-06-12 14:38:37 +0200

    Introduce classifier concept to model
    A classifier captures a single decision boundary between two classes. It
    supports iteration over the *relevant* support vectors.
    
    For the special case of biclassification, there's is only one classifier and the
    begin/end member functions of model are conditionally enabled for the sake of
    backwards compatibility. In general multi-classification, those are disabled and
    one has to obtain a classifier object through the model::classifier(Label,Label)
    member function which then features begin/end functions.

--- model.hpp
+++ model.hpp
@@ -40,7 +40,140 @@ namespace svm {
         typedef parameters<Kernel> parameters_t;
         typedef typename problem_t::input_container_type input_container_type;
         typedef Label label_type;
-        typedef std::pair<Label, Label> classifier_type;
+
+        struct classifier_type {
+
+            struct const_iterator {
+                using support_vec_type = std::conditional_t<problem_t::is_precomputed,
+                                                            input_container_type,
+                                                            data_view>;
+                using support_vec_ref = std::conditional_t<problem_t::is_precomputed,
+                                                           input_container_type const&,
+                                                           data_view>;
+                using value_type = std::pair<double, support_vec_type>;
+                using reference = std::pair<double const&, support_vec_ref> const;
+                using pointer = std::pair<double, support_vec_ref> const *;
+                using iterator_category = std::bidirectional_iterator_tag;
+
+                const_iterator & operator++ () {
+                    ++yalpha;
+                    ++sv;
+                    if (yalpha == yalpha_1_end) {
+                        yalpha = yalpha_2;
+                        sv = sv_2;
+                    }
+                    return *this;
+                }
+                const_iterator operator++ (int) {
+                    const_iterator old(*this);
+                    ++(*this);
+                    return old;
+                }
+                const_iterator & operator-- () {
+                    if (yalpha == yalpha_2) {
+                        yalpha = yalpha_1_end;
+                        sv = sv_1_end;
+                    }
+                    --yalpha;
+                    --sv;
+                    return *this;
+                }
+                const_iterator operator-- (int) {
+                    const_iterator old(*this);
+                    --(*this);
+                    return old;
+                }
+                double coef () const {
+                    return *yalpha;
+                }
+
+                template <typename Problem = problem_t,
+                        typename = std::enable_if_t<!Problem::is_precomputed>>
+                data_view support_vec () const {
+                    return data_view(*sv);
+                }
+
+                template <typename Problem = problem_t,
+                        typename = std::enable_if_t<Problem::is_precomputed>,
+                        bool dummy = false>
+                input_container_type const& support_vec () const {
+                    data_view permutation_index(*sv, 0);
+                    return prob[permutation_index.front()-1].first;
+                }
+
+                auto operator* () const {
+                    return std::make_pair(coef(), support_vec());
+                }
+
+                friend bool operator== (const_iterator lhs, const_iterator rhs) {
+                    return lhs.yalpha == rhs.yalpha && lhs.sv == rhs.sv;
+                }
+                friend bool operator!= (const_iterator lhs, const_iterator rhs) {
+                    return lhs.yalpha != rhs.yalpha || lhs.sv != rhs.sv;
+                }
+
+                friend struct classifier_type;
+
+            private:
+                const_iterator (double * yalpha_1, struct svm_node ** sv_1,
+                                double * yalpha_1_end, struct svm_node ** sv_1_end,
+                                double * yalpha_2, struct svm_node ** sv_2,
+                                problem_t const& prob)
+                    : yalpha(yalpha_1), sv(sv_1),
+                      yalpha_1_end(yalpha_1_end), sv_1_end(sv_1_end),
+                      yalpha_2(yalpha_2), sv_2(sv_2), prob(prob) {}
+                double * yalpha, * yalpha_1_end, * yalpha_2;
+                struct svm_node ** sv, **sv_1_end, **sv_2;
+                problem_t const& prob;
+            };
+
+            using iterator = const_iterator;
+
+            classifier_type (model const& parent, size_t k1, size_t k2)
+                : parent(parent), k1(k1), k2(k2) {
+                if (k1 > k2) {
+                    std::swap(k1, k2);
+                    std::swap(this->k1, this->k2);
+                }
+                size_t sum = 0;
+                for (size_t k = 0; k < model::nr_labels; ++k) {
+                    if (k == k1)
+                        k1_offset = sum;
+                    if (k == k2)
+                        k2_offset = sum;
+                    sum += parent.m->nSV[k];
+                }
+            }
+
+            const_iterator begin () const {
+                return const_iterator {
+                    parent.m->sv_coef[k2-1] + k1_offset,
+                    parent.m->SV + k1_offset,
+                    parent.m->sv_coef[k2-1] + k1_offset + parent.m->nSV[k1],
+                    parent.m->SV + k1_offset + parent.m->nSV[k1],
+                    parent.m->sv_coef[k1] + k2_offset,
+                    parent.m->SV + k2_offset,
+                    parent.prob
+                };
+            }
+
+            const_iterator end () const {
+                return const_iterator {
+                    parent.m->sv_coef[k1] + k2_offset + parent.m->nSV[k2],
+                    parent.m->SV + k2_offset + parent.m->nSV[k2],
+                    parent.m->sv_coef[k2-1] + k1_offset + parent.m->nSV[k1],
+                    parent.m->SV + k1_offset + parent.m->nSV[k1],
+                    parent.m->sv_coef[k1] + k2_offset,
+                    parent.m->SV + k2_offset,
+                    parent.prob
+                };
+            }
+
+        private:
+            size_t k1, k2;
+            size_t k1_offset, k2_offset;
+            model const& parent;
+        };
 
         static const size_t nr_labels = detail::label_traits<Label>::nr_labels;
         static const size_t nr_classifiers = nr_labels * (nr_labels - 1) / 2;
@@ -49,68 +182,11 @@ namespace svm {
                                                  double,
                                                  std::array<double, nr_classifiers>>;
 
+        using nSV_type = std::conditional_t<nr_labels == 2,
+                                            std::pair<size_t, size_t>,
+                                            std::array<size_t, nr_labels>>;
         using label_arr_t = std::array<label_type, nr_labels>;
         using classifier_arr_t = std::array<classifier_type, nr_classifiers>;
-
-        class const_iterator {
-        public:
-            const_iterator & operator++ () {
-                ++yalpha;
-                ++sv;
-                return *this;
-            }
-            const_iterator operator++ (int) {
-                const_iterator old(*this);
-                ++(*this);
-                return old;
-            }
-            const_iterator & operator-- () {
-                --yalpha;
-                --sv;
-                return *this;
-            }
-            const_iterator operator-- (int) {
-                const_iterator old(*this);
-                --(*this);
-                return old;
-            }
-            double coef () const {
-                return *yalpha;
-            }
-
-            template <typename Problem = problem_t,
-                      typename = std::enable_if_t<!Problem::is_precomputed>>
-            data_view support_vec () const {
-                return data_view(*sv);
-            }
-
-            template <typename Problem = problem_t,
-                      typename = std::enable_if_t<Problem::is_precomputed>,
-                      bool dummy = false>
-            input_container_type const& support_vec () const {
-                data_view permutation_index(*sv, 0);
-                return prob[permutation_index.front()-1].first;
-            }
-
-            auto operator* () const {
-                return std::make_pair(coef(), support_vec());
-            }
-
-            friend bool operator== (const_iterator lhs, const_iterator rhs) {
-                return lhs.yalpha == rhs.yalpha && lhs.sv == rhs.sv;
-            }
-            friend bool operator!= (const_iterator lhs, const_iterator rhs) {
-                return lhs.yalpha != rhs.yalpha || lhs.sv != rhs.sv;
-            }
-
-            friend model;
-        private:
-            const_iterator (double * yalpha, struct svm_node ** sv, problem_t const& prob)
-                : yalpha(yalpha), sv(sv), prob(prob) {}
-            double * yalpha;
-            struct svm_node ** sv;
-            problem_t const& prob;
-        };
 
         model () : prob(0), m(nullptr) {}
 
@@ -164,12 +240,26 @@ namespace svm {
         }
 
         classifier_arr_t classifiers () const {
-            auto ls = labels();
             classifier_arr_t ret;
+            auto ls = labels();
             auto it = ret.begin();
-            for (auto l1 = ls.begin(); l1 != ls.end(); ++l1)
-                for (auto l2 = l1; l2 != ls.end(); ++l2, ++it)
-                    *it = classifier_type(*l1, *l2);
+            for (size_t k1 = 0; k1 < nr_labels - 1; ++k1) {
+                for (size_t k2 = k1 + 1; k2 < nr_labels; ++k2, ++it) {
+                    *it = classifier_type(*this, k1, k2);
+                }
+            }
+        }
+
+        classifier_type classifier (Label l1, Label l2) const {
+            auto ls = labels();
+            size_t k1, k2;
+            for (size_t k = 0; k < nr_labels; ++k) {
+                if (l1 == ls[k])
+                    k1 = k;
+                if (l2 == ls[k])
+                    k2 = k;
+            }
+            return classifier_type {*this, k1, k2};
         }
 
         template <typename Problem = problem_t,
@@ -190,19 +280,30 @@ namespace svm {
             return std::make_pair(label, dec);
         }
 
-        const_iterator begin () const {
-            return const_iterator(m->sv_coef[0], m->SV, prob);
+        template <typename..., size_t NC = nr_classifiers,
+                  typename = std::enable_if_t<NC == 1>>
+        typename classifier_type::const_iterator begin () const {
+            return classifier_type {*this, 0, 1}.begin();
         }
-        const_iterator end () const {
-            return const_iterator(m->sv_coef[0] + m->l, m->SV + m->l, prob);
+
+        template <typename..., size_t NC = nr_classifiers,
+                  typename = std::enable_if_t<NC == 1>>
+        typename classifier_type::const_iterator end () const {
+            return classifier_type {*this, 0, 1}.end();
         }
 
         double rho () const {
             return m->rho[0];
         }
 
-        std::pair<size_t, size_t> nSV () const {
-            return {m->nSV[0], m->nSV[1]};
+        nSV_type nSV () const {
+            if (nr_labels == 2) {
+                return {m->nSV[0], m->nSV[1]};
+            } else {
+                nSV_type n;
+                std::copy(m->nSV, m->nSV + nr_labels, n.begin());
+                return n;
+            }
         }
 
         size_t dim () const {
